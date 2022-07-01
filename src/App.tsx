c896cb9f8bc0857caa038f073ef2dfa11341c110
Giovanni <giososa24@gmail.com>
2022-06-30 23:28:53 -0500

    feat: add AppBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
 import React, { Suspense } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Foo from "./components/Foo";
+import AppBar from "./components/AppBar/AppBar";
 
 const App = () => {
   return (
-    <Suspense fallback="cargando ...">
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Foo />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <Suspense fallback="loading...">
+      <AppBar />
     </Suspense>
   );
 };

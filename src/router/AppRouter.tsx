f58fb67573478d04e51198ca91e3a4b6d08b4658
Giovanni Sosa <giososa24@gmail.com>
2022-07-02 21:59:35 -0500

    fix: use HashRouter instead of BrowseRouter

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,23 +1,23 @@
 import React, { FC } from 'react'
 import { Box } from '@mui/material'
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom'
 import Home from 'pages/Home'
 import NotFound from 'pages/NotFound'
 import AppBar from 'components/AppBar'
 
 const AppRouter: FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AppBar>
         <Box display="flex" width="100%" height="100%" position="absolute">
           <Routes>
-            <Route path="/" element={<Navigate to="/cv-gio-sosa" />} />
-            <Route path="/cv-gio-sosa" element={<Home />} />
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<Home />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Box>
       </AppBar>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 

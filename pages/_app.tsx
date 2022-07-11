907d5dba9f754a1ac2142837bb366181fc7575ed
Giovanni <giososa24@gmail.com>
2022-07-10 22:32:19 -0500

    feat: add @mui cache

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,22 @@ import { ComponentType } from 'react'
 import 'styles/globals.css'
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
+import { CacheProvider } from '@emotion/react'
+import createCache from '@emotion/cache'
 import ThemeContextProvider from 'theme/ThemeContext'
+
+export const muiCache = createCache({
+  key: 'mui',
+  prepend: true,
+})
 
 const MyApp: ComponentType<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeContextProvider>
-      <Component {...pageProps} />
-    </ThemeContextProvider>
+    <CacheProvider value={muiCache}>
+      <ThemeContextProvider>
+        <Component {...pageProps} />
+      </ThemeContextProvider>
+    </CacheProvider>
   )
 }
 

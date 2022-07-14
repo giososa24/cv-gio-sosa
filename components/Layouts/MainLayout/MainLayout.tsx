02f9b389c93d7e9c0711b56fb04c9f0184623493
Giovanni Sosa <giososa24@gmail.com>
2022-07-13 23:33:08 -0500

    feat: add facebook metatags

--- components/Layouts/MainLayout/MainLayout.tsx
+++ components/Layouts/MainLayout/MainLayout.tsx
@@ -23,6 +23,15 @@ const MainLayout: FC<MainLayoutProps> = ({ children, section }) => {
         <meta name="copyright" content="Giovanni Abraham Sosa de la Cruz" />
         <meta name="robots" content="index, follow" />
         <link rel="icon" href="/favicon.ico" />
+
+        {/* Facebook metatags */}
+        <meta property="og:title" content="Gio Sosa - Home" />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/ddp9kx9ec/image/upload/v1657773094/69700020_2439957779384101_6064492451857432576_n_pbx3hd.jpg"
+        />
+        <meta property="og:url" content="https://gio-sosa.com" />
+        <meta property="og:description" content="Portafolio personal de Gio Sosa" />
       </Head>
 
       <Nabvar />

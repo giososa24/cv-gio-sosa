9c8d876a3df592a20decce3815ed5d17560477b0
Giovanni <giososa24@gmail.com>
2022-07-17 00:49:41 -0500

    style: add ActiveLink styles

--- components/Nabvar/components/ActiveLink/ActiveLink.tsx
+++ components/Nabvar/components/ActiveLink/ActiveLink.tsx
@@ -1,16 +1,17 @@
 import { FC } from 'react'
 import Link from 'next/link'
-// import { useRouter } from 'next/router'
+import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
 import ActiveLinkProps from './ActiveLink.types'
+import styles from './ActiveLink.module.css'
 
 const ActiveLink: FC<ActiveLinkProps> = ({ link, href }) => {
-  // const { asPath } = useRouter()
+  const { asPath } = useRouter()
   const { i18n } = useTranslation()
 
   return (
     <Link href={href} locale={i18n.language}>
-      <a>{link}</a>
+      <a className={asPath === href ? styles['link-active'] : styles.link}>{link}</a>
     </Link>
   )
 }

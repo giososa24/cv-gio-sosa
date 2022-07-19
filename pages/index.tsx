580ce87602e906425b2a3b12484920ef2917afbe
Giovanni <giososa24@gmail.com>
2022-07-18 22:19:11 -0500

    feat: add greeting without styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from 'next'
 import { GetStaticProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import Image from 'next/image'
+import { Box, Typography } from '@mui/material'
+import HelloHand from 'assets/images/hello-hand.png'
 import MainLayout from 'components/Layouts/MainLayout'
 import JsonCard from 'components/JsonCard/JsonCard'
 
@@ -8,6 +11,14 @@ const Home: NextPage = () => {
   return (
     <MainLayout section="Inicio">
       <JsonCard />
+      <Box mt="15px" display="flex" gap="10px">
+        <Box minWidth="150px">
+          <Image src={HelloHand} alt="hello-hand" width="150px" height="150px" />
+        </Box>
+        <Box>
+          <Typography>Hola, soy Giovanni Sosa</Typography>
+        </Box>
+      </Box>
     </MainLayout>
   )
 }

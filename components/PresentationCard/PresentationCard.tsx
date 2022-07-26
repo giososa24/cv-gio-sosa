98feddf5e3e14fff5b2fa4a517a0c08823c67d7a
Giovanni <giososa24@gmail.com>
2022-07-25 22:32:28 -0500

    feat: add presentation card

--- components/PresentationCard/PresentationCard.tsx
+++ components/PresentationCard/PresentationCard.tsx
@@ -1,45 +1,42 @@
-import React, { FC, useMemo } from 'react'
-import { Box } from '@mui/material'
-import Typewriter from 'typewriter-effect'
+import React, { FC } from 'react'
+import Image from 'next/image'
+import { Box, Typography } from '@mui/material'
 import { useTranslation } from 'next-i18next'
 import classes from './PresentationCard.module.css'
+import HelloHand from 'assets/images/hello-hand.png'
 
 const PresentationCard: FC = () => {
   const { t } = useTranslation('index')
 
-  const jsonText = useMemo(
-    () => `
-{<br />
-&nbsp;&nbsp;&nbsp;&nbsp;<span class="typewriter-tag">"${t(
-      'nombre'
-    )}"</span>: &nbsp;<span class="typewriter-label">"Gio Sosa"</span>,<br />
-&nbsp;&nbsp;&nbsp;&nbsp;<span class="typewriter-tag">"${t(
-      'puesto'
-    )}"</span>: &nbsp;<span class="typewriter-label">"${t('Desarrollador frontend')}"</span>,<br />
-}
-`,
-    [t]
-  )
-
   return (
-    <Box className={classes.container}>
-      <Box className={classes['typewritter-container']}>
+    <Box className={classes.wrapper}>
+      <Box className={classes.container}>
         <Box display="flex" flexDirection="row" gap="5px" m="0 -15px 10px">
           <Box className={classes.bullets} style={{ backgroundColor: 'red' }} />
           <Box className={classes.bullets} style={{ backgroundColor: 'yellow' }} />
           <Box className={classes.bullets} style={{ backgroundColor: 'green' }} />
         </Box>
-        <Typewriter
-          onInit={(typewriter): void => {
-            typewriter.typeString(jsonText).start()
-          }}
-          options={{
-            autoStart: true,
-            loop: true,
-            wrapperClassName: classes['type-writter'],
-            cursorClassName: classes['type-writter'],
-          }}
-        />
+        <Box className={classes['greeting-container']}>
+          <Box className={classes['hand-container']}>
+            <Image
+              src={HelloHand}
+              className={classes.handImage}
+              alt="hello-hand"
+              width="150px"
+              height="150px"
+            />
+          </Box>
+          <Box>
+            <Typography component="h1" className={classes.title}>
+              {t('Hola, soy Gio Sosa')}
+            </Typography>
+            <Typography className={classes.info}>
+              {t(
+                'La programación más que un trabajo es mi hobby, me especializo desarrollando frontend en React pero también cuento con conocimientos de backend y otras tecnologías'
+              )}
+            </Typography>
+          </Box>
+        </Box>
       </Box>
     </Box>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,14 @@
 import type { NextPage } from 'next'
 import { GetStaticProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import Image from 'next/image'
-import { Box, Typography } from '@mui/material'
-import { useTranslation } from 'next-i18next'
-import HelloHand from 'assets/images/hello-hand.png'
 import MainLayout from 'components/Layouts/MainLayout'
 import PresentationCard from 'components/PresentationCard'
-import classes from 'styles/index.module.css'
 import CardContent from 'components/CardContent'
 
 const Home: NextPage = () => {
-  const { t } = useTranslation('index')
-
   return (
     <MainLayout section="Inicio">
       <PresentationCard />
-      <Box className={classes['greeting-container']}>
-        <Box className={classes['hand-container']}>
-          <Image
-            src={HelloHand}
-            className={classes.handImage}
-            alt="hello-hand"
-            width="150px"
-            height="150px"
-          />
-        </Box>
-        <Box>
-          <Typography component="h1" className={classes.title}>
-            {t('Hola, soy Gio Sosa')}
-          </Typography>
-          <Typography className={classes.info}>
-            {t(
-              'La programación más que un trabajo es mi hobby, me especializo desarrollando frontend en React pero también cuento con conocimientos de backend y otras tecnologías'
-            )}
-          </Typography>
-        </Box>
-      </Box>
       <CardContent />
     </MainLayout>
   )

f8fed55e82714151539cffe8acce272acac63e6d
Giovanni Sosa <giososa24@gmail.com>
2022-07-27 21:12:02 -0500

    feat: aos setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
-import { ComponentType } from 'react'
+import { ComponentType, useEffect } from 'react'
 import 'styles/globals.css'
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 const MyApp: ComponentType<AppProps> = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    AOS.init()
+    AOS.refresh()
+  }, [])
+
   return (
     <ThemeProvider>
       <Component {...pageProps} />

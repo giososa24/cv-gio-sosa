69983286ea46b8b0cb4de4363181d2d58a716a74
Giovanni Sosa <giososa24@gmail.com>
2022-08-17 20:01:25 -0500

    feat: add frontend items

--- components/CardContent/frontEndItems.ts
+++ components/CardContent/frontEndItems.ts
@@ -3,6 +3,15 @@ import ReactLogo from 'assets/images/react-logo.png'
 import AngularLogo from 'assets/images/angular-logo.png'
 import ReactNativeLogo from 'assets/images/react-native-logo.png'
 import NextJsLogo from 'assets/images/nextjs-logo.png'
+import HTML5Logo from 'assets/images/html5-logo.png'
+import CSS3Logo from 'assets/images/css3-logo.png'
+import JQueryLogo from 'assets/images/jquery-logo.png'
+import BootstrapLogo from 'assets/images/bootstrap-logo.png'
+import MaterialUILogo from 'assets/images/material-ui-logo.png'
+import TailwindLogo from 'assets/images/tailwind-logo.png'
+import HugoLogo from 'assets/images/hugo-logo.png'
+import JSLogo from 'assets/images/js-logo.png'
+import TSLogo from 'assets/images/ts-logo.png'
 
 const frontEndItems: CollectionType = {
   title: 'Frontend',
@@ -30,6 +39,60 @@ const frontEndItems: CollectionType = {
       alt: 'angular-logo',
       width: '80px',
       heigth: '80px',
+    },
+    {
+      element: HTML5Logo,
+      alt: 'html5-logo',
+      width: '120px',
+      heigth: '120px',
+    },
+    {
+      element: CSS3Logo,
+      alt: 'css3-logo',
+      width: '120px',
+      heigth: '120px',
+    },
+    {
+      element: JQueryLogo,
+      alt: 'jquery-logo',
+      width: '120px',
+      heigth: '120px',
+    },
+    {
+      element: BootstrapLogo,
+      alt: 'bootstrap-logo',
+      width: '120px',
+      heigth: '100px',
+    },
+    {
+      element: MaterialUILogo,
+      alt: 'material-ui-logo',
+      width: '120px',
+      heigth: '120px',
+    },
+    {
+      element: TailwindLogo,
+      alt: 'tailwind-logo',
+      width: '200px',
+      heigth: '120px',
+    },
+    {
+      element: HugoLogo,
+      alt: 'hugo-logo',
+      width: '250px',
+      heigth: '120px',
+    },
+    {
+      element: JSLogo,
+      alt: 'js-logo',
+      width: '110px',
+      heigth: '120px',
+    },
+    {
+      element: TSLogo,
+      alt: 'ts-logo',
+      width: '110px',
+      heigth: '120px',
     },
   ],
 }

e4f6e78068c6ed88ec4a0f84ac66867fcad97ca9
Giovanni Sosa <49350865+giososa24@users.noreply.github.com>
2023-05-01 10:30:20 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 const nextConfig = {
   reactStrictMode: true,
   i18n,
-  webpack5: true,
+  //webpack5: true,
   webpack: config => {
     config.resolve.fallback = { fs: false }
     if (config.resolve.plugins) {
